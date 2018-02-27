a6f87da38b8869f723c36116bf6e455679342d73
mihailik <mihailik@gmail.com>
2018-02-27 22:44:49 +0000

    Let's put build in JS, no TS complexities.

--- build.js
+++ build.js
@@ -8,20 +8,12 @@ catch (error) {
 }
 
 process.stdout.write('mini terminal');
+
 try {
   var ts = require('typescript');
 }
 catch (error) {
-  console.log('ERR> TypeScript not installed');
-  console.log('ERR> Make sure to run npm install to ensure all dependencies are available.');
-  process.exit(1);
+  throw new Error(' TypeScript not installed (run npm install)');
 }
 
-process.stdout.write(' - build script');
-var buildScript = fs.readFileSync(path.resolve(__dirname, 'src/build-script/build-script.ts')) + '';
-process.stdout.write('..');
-var transpiledBuildScript = ts.transpile(buildScript);
-var transpileTime = Date.now() - startTime;
-console.log('. prepared in '+(transpileTime/1000)+'s.');
-
-eval(transpiledBuildScript);
+//TODO: build
---
+++ src/node/httpServer.ts
@@ -0,0 +1,13 @@
+namespace miterm {
+  
+  export declare namespace httpServer {
+    type Options = {
+      port: number;
+    }
+  }
+  
+  export function httpServer(options: httpServer.Options): Promise<httpServer> {
+    
+    //var s = httpServer(null);
+  }
+}
